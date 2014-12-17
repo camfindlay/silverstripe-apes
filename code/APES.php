3ff27fe48c38fd89de37366bc76830cf5f6515c4
Damian Mooyman <damian@silverstripe.com>
2014-12-17 15:01:34 +1300

    APE Refactor and cleanup

--- code/APES.php
+++ code/APES.php
@@ -3,116 +3,468 @@
 /**
  * Automated Provision for Email Services (APES)
  * 
- * When provided credentials and an array of Member fields to sync this will keep SilverStripe and Mailchimp in sync if a user has opted in to recevie emails.
+ * When provided credentials and an array of Member fields to sync this will keep SilverStripe and
+ * Mailchimp in sync if a user has opted in to recevie emails.
  *
  * @package    apes
  * @author     Cam Findlay <cam@silverstripe.com>
  */
 class APES extends DataExtension {
 
-    private static $mailchimp_api_key;
+	/**
+	 * Unsubscribe users on delete?
+	 *
+	 * @var boolean
+	 * @config
+	 */
+	private static $mailchimp_unsubscribe_on_delete = false;
 
-    private static $mailchimp_list_id;
+	/**
+	 * Require double-opt in for users (check email)
+	 *
+	 * @var type
+	 */
+	private static $double_optin = true;
 
-    private static $sync_member_fields = array();
+	/**
+	 * Mailchimp list ID
+	 *
+	 * Override by defining SS_MAILCHIMP_LIST_ID
+	 *
+	 * @var string
+	 * @config
+	 */
+	private static $mailchimp_list_id;
 
-    private static $double_optin = true;
+	/**
+	 * Field used for mailchimp options
+	 *
+	 * @var string
+	 * @config
+	 */
+	private static $mailchimp_form_prefix = 'MailChimp';
 
-    private static $db = array(
-        'MailchimpID' => 'Varchar'
-    );
+	/**
+	 * Get the list ID to use
+	 *
+	 * @return string
+	 */
+	public static function get_mailchimp_list_id() {
+		if(defined('SS_MAILCHIMP_LIST_ID')) return SS_MAILCHIMP_LIST_ID;
+		return $this->owner->config()->mailchimp_list_id;
+	}
 
-    public function updateCMSFields(FieldList $fields) {
-        $fields->addFieldToTab('Root.Mailchimp', ReadonlyField::create('MailchimpID'));
-    }
+	/**
+	 * Lis of member fields to synchronise
+	 *
+	 * Map of local field to mailchimp field
+	 *
+	 * @var array
+	 * @config
+	 */
+	private static $sync_member_fields = array(
+		'FirstName' => 'FNAME',
+		'Surname' => 'LNAME',
+		'Email' => 'EMAIL'
+	);
+	
+	private static $db = array(
+		'MailchimpID' => 'Varchar'
+	);
 
-    public function onBeforeWrite() {
-        //don't ping the API is the logged in user is an ADMIN
-        //if(Permission::check('ADMIN')){
-          //  return false;
-       // }
+	public function updateCMSFields(FieldList $fields) {
+		$fields->addFieldsToTab(
+			'Root.Mailchimp',
+			array(
+				ReadonlyField::create('MailchimpID', 'Mailchimp ID'),
+				ReadonlyField::create('ListStatus',  'List Status')
+			)
+		);
+	}
 
-        $changed = $this->owner->getChangedFields();
-        
-        //only do this if actual data has changed, not just LastEdited etc
-        //@todo test to see if adding a new rego from the CMS will trigger this?
-        //@todo if email has changed add this to the API call for changed email in MCAPI2
-        //if (count($changed) > 3) {
+	/**
+	 * Gets an array of data identifying this user with mailchimp
+	 *
+	 * @return array
+	 */
+	public function getMailchimpRef() {
+		if($this->owner->MailchimpID) {
+			return array('leid' => $this->owner->MailchimpID);
+		} else {
+			return array('email' => $this->owner->Email);
+		}
+	}
 
-            try {
-                $this->mc = new Mailchimp(Config::inst()->get('APES','mailchimp_api_key'));
-            } catch (Mailchimp_Error $e) {
-                user_error($e->getMessage(), E_USER_ERROR);
-            }
+	/**
+	 * Get selected groups for this user in a format suitable for sending to list->updateSubscriber
+	 *
+	 * @return array
+	 */
+	public function getMailChimpUserGroupings() {
+		// Map synchronised fields to mailchimp tags, ensuring to retrieve full profile
+		// prior to safely merge updated fields with existing ones
+		$memberInformation = $this->getMailChimpInformation();
+		if(empty($memberInformation['merges']['GROUPINGS'])) return array();
 
-            //@todo make this a SS ArrayList or DataList
-            $merge_tags = array(
-                'FNAME' => $this->owner->FirstName,
-                'LNAME' => $this->owner->Surname,
-                'EMAIL' => $this->owner->Email
-            );
+		// Map get group format to set group format
+		$userGroupings = array();
+		foreach($memberInformation['merges']['GROUPINGS'] as $groupings) {
+			$selectedOptions = array();
+			foreach($groupings['groups'] as $option) {
+				if($option['interested']) $selectedOptions[] = $option['name'];
+			}
+			$userGroupings[] = array(
+				'id' => $groupings['id'],
+				'name' => $groupings['name'],
+				'groups' => $selectedOptions
+			);
+		}
+		return $userGroupings;
+	}
 
-            //@todo document how this works
-            if (Config::inst()->get('APES','sync_member_fields')) {
-                foreach ($fields as $field) {
-                  $field = trim($field);
-                  $tag = strtoupper(substr($field, 0, 8));
-                  $merge_tags[$tag] = $this->owner->$field;
-                }
-            }
+	/**
+	 * Get selected options for a given group ID
+	 *
+	 * @param int $id Group ID to get options for
+	 * @return array
+	 */
+	public function getMailChimpUserGroupingOptions($id) {
+		// Find member data for this group
+		$memberData = $this->getMailChimpUserGroupings();
+		foreach($memberData as $grouping) {
+			if($grouping['id'] == $id) return $grouping['groups'];
+		}
+		return array();
+	}
 
-            //make sure the member did't unsubscribe 
-            //if (!$this->isUnsubscribed($this->owner->Email)) {
-                
-                //add them or update them in the list. 
-                //@todo add error handling
-                //@todo if the member has a mailchimp id use this over the email address.
-                $subscribe = $this->mc->lists->subscribe(
-                    Config::inst()->get('APES','mailchimp_list_id'), 
-                    array('email' => $this->owner->Email),
-                    $merge_tags, 
-                    'html', 
-                    $double_optin, 
-                    true, 
-                    true, 
-                    false
-                );
+	/**
+	 * Update this user on mailchimp
+	 *
+	 * @param array $groups Specify group data for merge, or null to leave unchanged
+	 */
+	public function subscribeMailChimpUser($groups = null) {
+		// Map synchronised fields to mailchimp tags, ensuring to retrieve full profile
+		// prior to safely merge updated fields with existing ones
+		$memberInformation = $this->getMailChimpInformation();
+		$mergeTags = $memberInformation && isset($memberInformation['merges'])
+			? $memberInformation['merges']
+			: array();
+		foreach ($this->owner->config()->sync_member_fields as $field => $tag) {
+			$mergeTags[$tag] = $this->owner->$field;
+		}
 
-                //return the unique Mailchimp ID for this member if user doesn't already have one
-                //@todo look at using this later to do 2 way sync
-                if(!$this->owner->MailchimpID){
-                    
-                    $this->owner->MailchimpID = $subscribe['leid']; 
-                }
-            //}
-            
-        //}
-       // parent::onBeforeWrite();
-    }
+		// Map get group format to set group format
+		$mergeTags['groupings'] = isset($groups)
+			? $groups
+			: $this->getMailChimpUserGroupings();
+		unset($mergeTags['GROUPINGS']);
 
-    public function onBeforeDelete() {
-        //remove the member from mailchimp if they ask to be deleted
-      //  $siteconfig = SiteConfig::current_site_config();
-      //  $api = new MCAPI($siteconfig->MailchimpApiKey);
-      //  $api->listUnsubscribe($siteconfig->MailchimpListId, $this->owner->Email);
-    }
+		//@todo add error handling
+		// Subscribe this user
+		try {
+			$api = SS_Mailchimp::instance();
+			$subscribe = $api->lists->subscribe(
+				static::get_mailchimp_list_id(),
+				$this->getMailchimpRef(),
+				$mergeTags,
+				'html',
+				$this->owner->config()->double_optin,
+				true, // update
+				true, // replace interests
+				false // dont send welcome
+			);
 
+			// Store the unique Mailchimp ID for this member if user doesn't already have one
+			//@todo look at using this later to do 2 way sync
+			if (!$this->owner->MailchimpID) {
+				$this->owner->MailchimpID = $subscribe['leid'];
+			}
+		} catch (Mailchimp_Error $exception) {
+			SS_Log::log($exception, SS_Log::ERR);
+		}
+	}
 
-    /**
-     * Checks to see if a given user has already unsubscribed from the list we want to add them to... if they have DO NOT re-subscribe them!
-     */
-    protected function isUnsubscribed($email_address) {
-            $memberinfo = $this->mc->lists->memberInfo(
-                            Config::inst()->get('APES','mailchimp_list_id'), 
-                            $this->owner->Email
-            );
+	/**
+	 * Unsubscribe this user from mailchimp
+	 */
+	public function unsubscribeMailChimpUser() {
+		try {
+			$api = SS_Mailchimp::instance();
+			$api->lists->unsubscribe(
+				static::get_mailchimp_list_id(),
+				$this->getMailchimpRef()
+			);
+		} catch (Mailchimp_Error $exception) {
+			SS_Log::log($exception, SS_Log::ERR);
+		}
+	}
 
-        //now check the status variable.
-        if ($memberinfo['status'] == 'unsubscribed') {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public function onBeforeWrite() {
+		
+		// Only update ID if this record has been changed
+		$changed = $this->owner->getChangedFields();
+		if (empty($changed) && $this->owner->exists()) {
+			return;
+		}
+
+		// Only update members which are subscribed
+		if(!$this->isSubscribed()) return;
+
+		// Send subscription update to mailchimp
+		$this->subscribeMailChimpUser();
+		
+	}
+
+	public function onBeforeDelete() {
+		// Unsubscribe this user before deleting
+		if ($this->owner->config()->mailchimp_unsubscribe_on_delete && $this->isSubscribed()) {
+			$this->unsubscribeMailChimpUser();
+		}
+	}
+
+	/**
+	 * Determine the subscription status for this user
+	 *
+	 * @return string|false The list status, or false if not on the list
+	 */
+	public function getListStatus() {
+		$data = $this->getMailChimpInformation();
+		if($data) return $data['status'];
+		else return false;
+	}
+
+	/**
+	 * Return true if this user is on a list, and happily subscribed
+	 *
+	 * @return boolean
+	 */
+	public function isSubscribed() {
+		return $this->getListStatus() === 'subscribed';
+	}
+
+	public function isPending() {
+		return $this->getListStatus() === 'pending';
+	}
+
+	/**
+	 * Checks to see if a given user has already unsubscribed from the list we want to add them to...
+	 * If they have DO NOT re-subscribe them!
+	 *
+	 * @return boolean
+	 */
+	protected function isUnsubscribed() {
+		return $this->getListStatus() === 'unsubscribed';
+	}
+
+	/**
+	 * Gets group data for this list
+	 *
+	 * @return array
+	 */
+	public function getMailchimpListGroupings() {
+		// Return cached value
+		if(isset($this->owner->mcgroupdata)) return $this->owner->mcgroupdata;
+
+		// Request from API
+		try {
+			$groupData = SS_Mailchimp::instance()
+				->lists
+				->interestGroupings(static::get_mailchimp_list_id());
+		} catch (Mailchimp_Error $exception) {
+			SS_Log::log($exception, SS_Log::ERR);
+			return array();
+		}
+		// Sort data
+		return $this->owner->mcgroupdata = $this->sortData($groupData);
+	}
+
+	/**
+	 * Get data on this user's subscription status, or false if not subscribed
+	 *
+	 * @return array|false
+	 */
+	public function getMailChimpInformation() {
+		// Skip singletons
+		if(empty($this->owner->Email)) return false;
+
+		// Return cached value
+		if(isset($this->owner->mcdata)) return $this->owner->mcdata;
+
+		try {
+			$api = SS_Mailchimp::instance();
+			$result = $api->lists->memberInfo(
+				static::get_mailchimp_list_id(),
+				array(
+					$this->getMailchimpRef()
+				)
+			);
+		} catch (Mailchimp_Error $exception) {
+			SS_Log::log($exception, SS_Log::ERR);
+			return $this->owner->mcdata = false;
+		}
+
+		// If not on list, not subscribed
+		if($result && $result['success_count'] > 0) {
+
+			// If on lit, check the status variable.
+			foreach($result['data'] as $memberData) {
+				return $this->owner->mcdata = $memberData;
+			}
+		}
+
+		// Non-member
+		return $this->owner->mcdata = false;
+	}
+
+	/**
+	 * Helper function for sorting mailchimp standard lists
+	 *
+	 * @param array $data
+	 * @return array
+	 */
+	protected function sortData($data) {
+		usort($data, function($left, $right) {
+			return $left['display_order'] - $right['display_order'];
+		});
+		return $data;
+	}
+
+	/**
+	 * Gets the list of groups, and their subscriptions
+	 *
+	 * @link https://apidocs.mailchimp.com/api/1.2/listinterestgroups.func.php
+	 *
+	 * @return array List of groups as an associative array, each item containing the following keys:
+	 * <ul>
+	 * <li>id - ID of this group</li>
+	 * <li>name - Name of this group</li>
+	 * <li>type - One of checkboxes, radio, select, hidden</li>
+	 * <li>options - List of options, each with 'id', 'name', and 'selected' fields</li>
+	 * <li>selected - Helper list of selected option ids for this group</li>
+	 * <li>values - Helper array mapping of id to option names</li>
+	 * </dl>
+	 */
+	public function getMailChimpGroups() {
+		$groups = array();
+		$groupData = $this->getMailchimpListGroupings();
+		foreach($groupData as $groupDataItem) {
+			$groupID = $groupDataItem['id'];
+			$selectedOptions = $this->getMailChimpUserGroupingOptions($groupID);
+
+			// Save all options
+			$options = array();
+			$optionValues = array();
+			$optionData = $this->sortData($groupDataItem['groups']);
+			foreach($optionData as $optionDataItem) {
+				// Add option
+				$options[] = array(
+					'id' => $optionDataItem['id'],
+					'name' => $optionName = $optionDataItem['name'],
+					'selected' => in_array($optionName, $selectedOptions)
+				);
+				$optionValues[$optionName] = $optionName;
+			}
+
+			// Build the group
+			$groups[] = array(
+				'id' => $groupID,
+				'name' => $groupDataItem['name'],
+				'type' => $groupDataItem['form_field'],
+				'options' => $options,
+				'selected' => $selectedOptions, // selected options
+				'values' => $optionValues // all options
+			);
+		}
+		return $groups;
+	}
+
+	/**
+	 * Get the composite field necessary for generating the subscription options
+	 *
+	 * @return FormField
+	 */
+	public function getMailChimpField() {
+		// Add subscription settings for mailchimp
+		// Get list of available groups
+		// @see APES::getAvailableGroups
+		$prefix = $this->owner->config()->mailchimp_form_prefix;
+
+		// Show notice if pending
+		if($this->isPending()) {
+			return LiteralField::create(
+				"{$prefix}[Pending]",
+				"<p class='mailchimp-pending'>Your subscription is currently pending email verification</p>"
+			);
+		}
+
+		$groupFields = CompositeField::create(
+			// Flag to indicate these fields should be processed by the response handler
+			HiddenField::create("{$prefix}[Enabled]", false, 1)
+		)->addExtraClass('mailchimp-options');
+
+		// Generate fields for all groups
+		$groups = $this->getMailChimpGroups();
+		foreach($groups as $mailGroup) {
+			$groupID = $mailGroup['id'];
+
+			// Determine field to use
+			if($mailGroup['type'] === 'checkboxes') {
+				$groupFields->push(
+					CheckboxSetField::create(
+						"{$prefix}[Groups][{$groupID}]",
+						$mailGroup['name'],
+						$mailGroup['values'],
+						$mailGroup['selected']
+					)->addExtraClass('mailchimp-option')
+				);
+			} else {
+				// NOOP - not implemented
+			}
+		}
+
+		// Create subscription section
+		return CompositeField::create(
+			CheckboxField::create(
+				"{$prefix}[Subscribed]",
+				"I wish to subscribe for email updates",
+				$this->isSubscribed()
+			)->addExtraClass('mailchimp-subscribe'),
+			$groupFields
+		)->addExtraClass('mailchimp-fields');
+	}
+
+	/**
+	 * Update subscription details from a form containing the field generated by
+	 * {@see APES::getSubscriptionFields()}
+	 *
+	 * @param SS_HTTPRequest $request
+	 */
+	public function updateMailChimpSubscription($request) {
+		$prefix = $this->owner->config()->mailchimp_form_prefix;
+		$postedData = $request->postVar($prefix);
+
+		// Abort if no data from MailChimpField
+		if(empty($postedData['Enabled'])) return;
+
+		// Check if subscribing or not
+		if(empty($postedData['Subscribed'])) {
+			$this->unsubscribeMailChimpUser();
+			return;
+		}
+
+		// Generate list of groups
+		$groups = array();
+		$groupData = $this->getMailchimpListGroupings();
+		foreach($groupData as $groupDataItem) {
+			// Skip fully unselected groups
+			if(empty($postedData['Groups'][$groupDataItem['id']])) continue;
+			$groups[] = array(
+				'id' => $groupDataItem['id'],
+				'name' => $groupDataItem['name'],
+				'groups' => array_values($postedData['Groups'][$groupDataItem['id']])
+			);
+		}
+		$this->subscribeMailChimpUser($groups);
+	}
 
 }
